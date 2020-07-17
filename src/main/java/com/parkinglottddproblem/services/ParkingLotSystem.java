f51890cd12320c480368501c700c30e5c2cb3327
kajal <tiwarikajal1306@gmail.com>
2020-07-17 23:24:25 +0530

    ADDED Test case for vehical park

---
+++ src/main/java/com/parkinglottddproblem/services/ParkingLotSystem.java
@@ -0,0 +1,14 @@
+package com.parkinglottddproblem.services;
+
+public class ParkingLotSystem {
+
+
+    public void park(String car) {
+
+    }
+
+    public boolean isVehicleParked() {
+        return true;
+    }
+}
+
--- src/test/java/parkinglottddproblem/ParkingLotTest.java
+++ src/test/java/parkinglottddproblem/ParkingLotTest.java
@@ -1,5 +1,17 @@
 package parkinglottddproblem;
 
+import com.parkinglottddproblem.services.ParkingLotSystem;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotTest {
 
+ParkingLotSystem parkingLotSystem;
+
+@Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+    parkingLotSystem.park("Car");
+    boolean isVehicleParked = parkingLotSystem.isVehicleParked();
+    Assert.assertTrue(isVehicleParked);
+}
 }
